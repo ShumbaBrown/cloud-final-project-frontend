ca11476134f9bf29598eec3dd75ebfd2e6839648
shumbabrown <shumbabrownjc@gmail.com>
2019-04-08 13:20:46 -0700

    server working

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const app = express();
 app.get('/', (req, res) => {
   res
     .status(200)
-    .send('Hello, world!')
+    .send('Check console to see response from backend.')
     .end();
 });
 
